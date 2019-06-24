d73d4ed694a0c055e96d087e5766ce499bcfab70
David Inyangson <dinyangs@andrew.cmu.edu>
2019-06-23 17:38:38 -0700

    Initial Calendar and Organization models

--- src/main_calendar/models.py
+++ src/main_calendar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class  Calendar(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    timezone = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    organization_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Organization(models.Model):
+    name = models.CharField(max_length=200)
+    org_type = models.CharField(max_length=200)
+    mailing_address = Models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
 
 # Create your models here.
