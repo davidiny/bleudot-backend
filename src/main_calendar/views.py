78bb66f1ac2cb1caa8122281bd64e84391841fea
David Inyangson <dinyangs@andrew.cmu.edu>
2019-09-21 13:25:20 -0400

    Created basic calendar controller

--- src/main_calendar/views.py
+++ src/main_calendar/views.py
@@ -3,3 +3,13 @@
 # from .serializers import UserSerializer, OrganizationSerializer
 
 # Create your views here.
+
+from .models import Calendar
+from .serializers import CalendarSerializer
+from rest_framework import generics
+
+class CalendarList(generics.ListCreateAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+
+
