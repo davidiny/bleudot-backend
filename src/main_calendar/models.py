f2dca5c07607e2deef7137d7545b7a32ab87b500
virenbajaj <virenbajaj4@gmail.com>
2019-10-27 19:17:15 -0400

    removed redundant field from oraganization model, added django_extensions module to settings

--- src/main_calendar/models.py
+++ src/main_calendar/models.py
@@ -67,7 +67,6 @@ class Organization(models.Model):
     name            = models.CharField(max_length=200)
     org_type        = models.CharField(max_length=200)
     email           = models.EmailField(max_length=200)
-    org_type = models.CharField(max_length=200)
     created_at      = models.DateTimeField(auto_now_add=True)
 
 class Subscription(models.Model):
