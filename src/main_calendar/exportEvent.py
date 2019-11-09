3f4181073fe0f021d100d566f2d9e02637162cb9
David Inyangson <dinyangs@andrew.cmu.edu>
2019-11-09 16:33:20 -0500

    credentials stuff

--- src/main_calendar/exportEvent.py
+++ src/main_calendar/exportEvent.py
@@ -33,7 +33,7 @@ def exportEvent(summary):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES)
+                os.environ['CREDENTIALS'], SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
